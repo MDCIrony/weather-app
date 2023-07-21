8331af973e7f4783a07c0ec5293458c66c54df0a
MDCIrony <miguel.acastillodiaz@outlook.es>
2023-07-20 20:09:03 -0500

    feat: Added img view to weather by city detail

--- src/app/detail/[city_id]/page.tsx
+++ src/app/detail/[city_id]/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import getWeatherData from '@/services/remote/getWeatherData';
+import { Fragment } from 'react';
 
 interface Params {
   params: { city_id: string };
@@ -7,24 +9,29 @@ interface Params {
 
 export default async function Detail({ params }: Params): Promise<React.ReactElement> {
   const weatherInfo = await getWeatherData(params.city_id);
+  const iconUrl = `https://openweathermap.org/img/wn/${weatherInfo.weather.weather[0].icon}@2x.png`;
   return (
     <>
       <main>
         <div className="container">
           <Link href={'/'}>&larr; Home</Link>
           {weatherInfo && (
-            <h1>
-              {weatherInfo.city.name} ({weatherInfo.city.country})
-            </h1>
-          )}
-          {weatherInfo && (
-            <>
+            <Fragment>
+              <h1>
+                {weatherInfo.city.name} ({weatherInfo.city.country})
+              </h1>
               <h2>
                 {weatherInfo.weather.main.temp_max.toFixed(0)}&deg;C{' '}
                 {weatherInfo.weather.main.temp_min.toFixed(0)}&deg;C
               </h2>
               <div>{weatherInfo.weather.weather[0].description}</div>
-            </>
+              <Image
+                src={iconUrl}
+                width={50}
+                height={50}
+                alt={`${weatherInfo.city.name} Weather Icon`}
+              />
+            </Fragment>
           )}
         </div>
       </main>
