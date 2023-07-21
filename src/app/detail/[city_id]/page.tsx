600df10727a1e529674a51529f08994d751a8d62
MDCIrony <miguel.acastillodiaz@outlook.es>
2023-07-20 20:22:16 -0500

    feat: Styled weather city page

--- src/app/detail/[city_id]/page.tsx
+++ src/app/detail/[city_id]/page.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import getWeatherData from '@/services/remote/getWeatherData';
-import { Fragment } from 'react';
 
 interface Params {
   params: { city_id: string };
@@ -12,27 +11,43 @@ export default async function Detail({ params }: Params): Promise<React.ReactEle
   const iconUrl = `https://openweathermap.org/img/wn/${weatherInfo.weather.weather[0].icon}@2x.png`;
   return (
     <>
-      <main>
-        <div className="container">
-          <Link href={'/'}>&larr; Home</Link>
-          {weatherInfo && (
-            <Fragment>
-              <h1>
-                {weatherInfo.city.name} ({weatherInfo.city.country})
-              </h1>
-              <h2>
-                {weatherInfo.weather.main.temp_max.toFixed(0)}&deg;C{' '}
-                {weatherInfo.weather.main.temp_min.toFixed(0)}&deg;C
-              </h2>
-              <div>{weatherInfo.weather.weather[0].description}</div>
-              <Image
-                src={iconUrl}
-                width={50}
-                height={50}
-                alt={`${weatherInfo.city.name} Weather Icon`}
-              />
-            </Fragment>
-          )}
+      <main className="mt-5 mx-5">
+        <h1 className="text-xl font-medium mb-4">WeatherWise</h1>
+        <Link href="/" className="text-sm">
+          &larr; Home
+        </Link>
+        <div className="py-5">
+          <div className="bg-blue-500 rounded p-4">
+            <div className="grid grid-cols-2">
+              {weatherInfo && (
+                <>
+                  <div>
+                    <h2 className="text-2xl mb-4 text-white">
+                      {weatherInfo.city.name} ({weatherInfo.city.country})
+                    </h2>
+                    <span className="font-medium text-lg text-white">
+                      {weatherInfo.weather.main.temp_max.toFixed(0)}&deg;C
+                    </span>
+                    <span className="text-gray-300 text-sm">
+                      {weatherInfo.weather.main.temp_min.toFixed(0)}&deg;C
+                    </span>
+                  </div>
+
+                  <div className="justify-self-end">
+                    <Image
+                      src={iconUrl}
+                      width={60}
+                      height={60}
+                      alt={`${weatherInfo.city.name} Weather Icon`}
+                    />
+                    <div className="text-white text-sm">
+                      {weatherInfo.weather.weather[0].description}
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
         </div>
       </main>
     </>
